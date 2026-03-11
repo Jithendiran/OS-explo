8ca22f85eb264eb73bb9474462688251b0a8879e
Jithendiran <jidesh0410@gmail.com>
2026-03-11 05:39:27 +0530

    multi task completed

--- 80386/programs/simple_multi_task/head.asm
+++ 80386/programs/simple_multi_task/head.asm
@@ -1,12 +1,13 @@
 [bits 32]
 [org 0x0000]
 
-LATCH    equ 11930
 SCRN_SEL equ 0x18
-LDT0_SEL equ 0x28
+LDT0_SEL equ 0x28   
+TSS0_SEL equ 0x20  
+
 LDT1_SEL equ 0x38
-TSS0_SEL equ 0x20
 TSS1_SEL equ 0x30
+
 
 head_start:
     mov eax, 0x10   ; load data segment
@@ -14,7 +15,8 @@ head_start:
     lss esp, [init_stack]
     
     call setup_idt
-    call setup_gdt
+
+    lgdt [lgdt_opcode]
     
     mov eax, 0x10
     mov ds, ax
@@ -22,70 +24,170 @@ head_start:
     mov fs, ax
     mov gs, ax
     lss esp, [init_stack]
+    
+    ;-----------------------------------Modify IDT    
+    ; timer interrupt gate in IDT as item 8
+    lea ebx,timer_interrupt             ;32bit address
 
-;--------------------------------------------timer
-    mov al, 0x36
-    mov edx, 0x43
-    out dx, al
-    mov eax, LATCH
-    mov edx, 0x40
-    out dx, al
-    mov al, ah
-    out dx, al
-; interrupt date in IDT as item 8
-    mov eax, 0x00080000
-    mov ax, timer_interrupt
-    mov dx, 0x8E00
-    mov ecx, 0x08
-    lea esi, [idt + ecx*8]
+    mov eax, 0x00080000             
+    mov edx, ebx                        ;holds full address   
+
+    mov ax, bx                          ; 0008xxxx 
+    mov dx, 0x8E00                      ; xxxx8E00
+
+    lea esi, [idt + 0x08*8]             ; 8th entry, each eight byte size 
     mov [esi], eax
     mov [esi+4], edx
-; Set the system call trap gate descriptor at item 128 (0x80) of the IDT table
-    mov ax, system_interrupt
-    mov dx, 0xef00
-    mov ecx, 0x08
-    lea esi, [idt + ecx*8]
+
+    ;---------------------------
+    ; system interrupt
+
+    lea ebx,system_interrupt             ;32bit address
+
+    mov eax, 0x00080000             
+    mov edx, ebx                        ;holds full address   
+
+    mov ax, bx                          ; 0008xxxx 
+                                        ; DPL = 11 and Type = Trap
+    mov dx, 0xEF00                      ; xxxxEF00
+
+    lea esi, [idt + 0x20*8]             ; 32th entry, each eight byte size 
     mov [esi], eax
     mov [esi+4], edx
-;------------
+
+    sti                                 ; only work if sti placed before pushing EFLAGS; check comment at 80
+
+    ;-------------------------------Flag setup
+    ; clearing NT flag
     pushf
-    and dword [esp], 0xffffbfff
+    and dword [esp], 0xffffbfff 
     popf
+
+    ;-------------------------------Ragister setup
+    ; load task register to task   
     mov eax, TSS0_SEL
-    ltr ax
+    ltr ax                   
+
+    ; load LDT
     mov eax, LDT0_SEL
     lldt word ax
-    mov dword [current], 0
-    sti
 
-    push 0x17
-    push init_stack
-    pushf
-    push 0x0f
-    push task0
+    ;------------------------------Setup 
+    push 0x17           ;   SS          ;; 0001_0111 -> Index = 0001_0(2) T=1 DPL=11
+    push init_stack     ;   ESP
+    pushf               ;   EFLAGS
+                        ;       Index LDT   RLP
+    push 0x0f           ;   CS  00001 1     11
+    push task0          ;   EIP
+    
     iret
-;--------------------------------------------------------------------
 
-setup_gdt:
-    lgdt [lgdt_opcode]
-    ret
+nop
+
+;======================================================================================
 
 setup_idt:
-    lea edx, ignore_int ; edx holds the full 32-bit address of ignore_int eg: 0x12345678
-    mov eax, 0x00080000 ; eax has 0x00080000, top bits are selector
-    mov ax, dx          ; take the bottom 16 bits of the handler address (0x5678) and put them in the bottom of eax. eax = 0x00085678
-    mov dx, 0x8E00      ; Interrupt gate type is 14, plevel is 0. edx = 0x12348E00
-    lea edi, idt        ; edi holds the IDT starting address
-    mov ecx, 256        ; ecx holds the count 
+    lea edx, ignore_int 
+    mov eax, 0x00080000 
+    mov ax, dx          
+    mov dx, 0x8E00      
+    lea edi, idt        
+    mov ecx, 35         
 rp_idt:
-    mov [edi], eax      ; eax = 0x00085678, base = 0008, limit = 0x5678
-    mov [edi+4], edx    ; edx = 0x12348E00  move 4 bytes, write limit = 0x1234 and perm, flags = 8E00
-    add edi, 8          ; move to next IDT entry
-    dec ecx             ; reduce counter
-    jne near rp_idt          ; if exc is non zero, repeate
+    mov [edi], eax      
+    mov [edi+4], edx    
+    add edi, 8          
+    dec ecx             
+    jne near rp_idt     
     lidt [lidt_opcode]
     ret
 
+nop
+
+system_interrupt:
+    push ds                 ; Save the user's data segmen 
+    pusha                   ; Save all general registers
+
+    ; Load Kernel Data Segment
+    mov edx, 0x10
+    mov ds, dx
+
+    call write_char         ; write_char uses the char in AL
+
+    popa                    ; Restore general registers
+    pop ds                  ; Restore the user's data segment
+    iret                    ; Return from interrupt
+
+nop
+
+ignore_int:
+    push ds
+    push eax
+
+    mov eax, 0x10       ; Kernel Data Segment
+    mov ds, ax
+
+    mov eax, 'I'        ; pass `I` as parameter
+    call write_char
+
+    pop eax
+    pop ds
+    iret
+
+nop
+
+; This is the timer interrupt handler. The main function is to perform task switching operations.
+align 4
+timer_interrupt:
+
+    push ds
+    push eax
+
+    mov eax, 0x10       ; Kernel Data Segment
+    mov ds, ax
+
+    mov eax, 'T'        
+    call write_char
+
+     ; --- Important --- ; without this clock interrupt won't happen
+    mov al, 0x20        ; EOI command
+    out 0x20, al        ; Send to Master PIC
+    ; ---------------------
+
+
+    ; check current task
+    mov eax, 1
+    cmp [current], eax
+
+    je  .switch_0           ; if eax == 1, jmp to switch_0
+
+.switch_1:
+
+    mov [current], eax      ; current task is 0, switch to 1
+
+    mov eax, 'X'
+    call write_char
+    ;jmp $
+
+    jmp  TSS1_SEL:0x00       ; CPU ignore this offset 0x00, take value from TSS1
+
+    jmp .end                ; When return to Task 0, jump to cleanup
+
+.switch_0:
+    mov dword [current], 0        ; switch to 0
+
+    mov eax, 'y'
+    call write_char
+    ;jmp $
+
+    jmp  TSS0_SEL:0x00       ; CPU ignore this offset 0x00, take value from TSS0
+
+.end:
+    pop eax
+    pop ds
+    iret
+
+nop
 
 write_char:
 
@@ -117,186 +219,158 @@ write_char:
     pop  eax
     pop  gs
     ret
+nop    
 
-    
+;============================================================================================
 
-; The following are 3 interrupt handlers: default, timer, and system call interrupt.
+lidt_opcode:
+    dw (idt_end - idt)-1    
+    dd  idt             
 
-; Ignore_int is default handler. If system generates other interrupts, it display char 'C'.
-align 4
-ignore_int:
-    push ds
-    push eax
+nop
 
-    mov eax, 0x10       ; Kernel Data Segment
-    mov ds, ax
+lgdt_opcode:
+    dw (end_gdt-gdt)-1   
+    dd gdt             
 
-    mov eax, 67         ; pass `C` as parameter
-    call write_char
-
-    pop eax
-    pop ds
-    iret
-
-; This is the timer interrupt handler. The main function is to perform task switching operations.
-align 4
-timer_interrupt:
-
-    push ds
-    push eax
-
-    ; switch kernel data segment
-    mov eax, 0x10           
-    mov ds, ax
-
-    ; Then send EOI to 8259A to allow other interrupts.
-    mov al, 0x20
-    out 0x20, al
-
-    ; check current task
-    mov eax, 1
-    cmp [current], eax
-
-    je  .switch_0           ; if eax == 1, jmp to switch_0
-
-.switch_1:
-    mov [current], eax      ; current task is 0, switch to 1
-    jmp  TSS1_SEL:0x00       ; CPU ignore this offset 0x00, take value from TSS1
-
-    jmp .end                ; When return to Task 0, jump to cleanup
-
-.switch_0:
-    mov dword [current], 0        ; switch to 0
-    jmp  TSS0_SEL:0x00       ; CPU ignore this offset 0x00, take value from TSS0
-
-.end:
-    pop eax
-    pop ds
-    iret
-
-; The system call int 0x80 handler. This example has only one display char function.
-align 4
-system_interrupt:
-    push ds                 ; Save the user's data segmen 
-    pusha                   ; Save all general registers
-
-    ; Load Kernel Data Segment
-    mov edx, 0x10
-    mov ds, dx
-
-    call write_char         ; write_char uses the char in AL
-
-    popa                    ; Restore general registers
-    pop ds                  ; Restore the user's data segment
-    iret                    ; Return from interrupt
-
-
-;/********************************************************************************/
-current:    ; 00000181 - 00000184
-    dd 0
-scr_loc:    ; 00000185 - 00000187
-    dd 0
+nop
 
 
 ; empty IDT
-align 4
-lidt_opcode:
-    dw (256*8)-1    ; limit (07FF)
-    dd  idt         ; base address (000198)
-lgdt_opcode:
-    dw (end_gdt-gdt)-1  ; limit (3f00)
-    dd gdt              ; base address (000398)
-
-align 8
-idt:        ; 00000198
-    times 256 dq 0
-
-;------------------------------------
-gdt:
-    dq 0x0000000000000000
-    dq 0x00c0_9a00_0000_07ff                ; Kernel code segment, (00000000-007FFFFF) 8MiB
-    dq 0x00c0_9200_0000_07ff                ; kernel stack segment, (00000000-007FFFFF) 8MiB
-    dq 0x00c0_920b_8000_0002                ; Display buffer, ( 000b8000 - 0x000BAFFF) 12 KiB
-    dw 0x68, tss0, 0xe900, 0x00             ; DLP = 11, TSS, limit is 0x68 == 104bytes
-    dw 0x40, ldt0, 0xe200, 0x0              ; DLP = 11, LDT, limit is 0x40 == 64bytes  
-    dw 0x68, tss1, 0xe900, 0x00             ; DLP = 11, TSS, limit is 0x68 == 104bytes
-    dw 0x40, ldt1, 0xe200, 0x0              ; DLP = 11, LDT, limit is 0x40 == 64bytes 
-end_gdt:                                        
+idt:        
+    times 35 dq 0
+idt_end:
 
 nop
-;-----------------------------------------------------------------------------------
-times 128 dd 0          ; 128 * 4 = 512 ; 512 bytes stack space
+
+gdt:
+    dq 0x0000000000000000                   ;0
+    dq 0x00c0_9a00_0000_07ff                ;1; Kernel code segment, (00000000-007FFFFF) 8MiB
+    dq 0x00c0_9200_0000_07ff                ;2; kernel stack segment, (00000000-007FFFFF) 8MiB
+    dq 0x00c0_920b_8000_0002                ;3; Display buffer, ( 000b8000 - 0x000BAFFF) 12 KiB
+
+    ; task0
+    dw 0x68, tss0, 0xe900, 0x00             ;4; DLP = 11, TSS, limit is 0x68 == 104bytes
+    dw 0x40, ldt0, 0xe200, 0x0              ;5; DLP = 11, LDT, limit is 0x40 == 64bytes  
+    ; task1
+    dw 0x68, tss1, 0xe900, 0x00             ;6; DLP = 11, TSS, limit is 0x68 == 104bytes
+    dw 0x40, ldt1, 0xe200, 0x0              ;7; DLP = 11, LDT, limit is 0x40 == 64bytes
+
+end_gdt:
+nop
+
+;============================================================================================
+
+scr_loc:
+    dd 0
+nop
+
+current:    
+    dd 0
+nop
+
+;============================================================================================
+
+times 128 dq 0          ; 128 * 4 = 512 ; 512 bytes stack space
 
 init_stack:
     dd init_stack   ; Stack segment offset position
     dw 0x10         ; Stack segment, same as kernel data seg
 
-
-;============================================================== Task 0 LDT and TSS
+nop
+;============================================================================================
 
 align 8
 ldt0: 
     dq 0x0000000000000000
-    dq 0x00c0fa00000003ff        ; base = 00 ; g=1 x=1 0 avl=0   limit=0| p = 1 DLP=11 s=1 type code=1 conf=0 e/r= 1 a=0          base=00     |  base = 0000 limit = 03ff
-    dq 0x00c0f200000003ff        ; stack segment
 
+    dq 0x00c0fa00000003ff        ; base = 00 ; g=1 x=1 0 avl=0 limit=0| p = 1 DLP=11 s=1 type code=1 conf=0 e/r= 1 a=0          base=00 |  base = 0000 limit = 03ff  => 03ff=1023 * 4096 = 4190208 == 3FF000 + FFF = 3FFFFF; 00000000-003FFFFF
+    dq 0x00c0f200000003ff        ; stack segment
+; Since it's base address also 00, it is using the same code 
+nop
+
+; tss0 content are 0 because segment , general register will taken from IRET
 tss0:
     dd 0                    ; back link
     dd krn_stk0, 0x10       ; esp0, ss0
-    dd 0, 0, 0, 0, 0        ; esp1, ss1, esp2, ss2, cr3
+    dd 0, 0, 0, 0, 0        ; esp1, ss1, esp2, ss2, cr3     
     dd 0, 0, 0, 0, 0        ; eip, eflags, eax, ecx, edx
     dd 0, 0, 0, 0, 0        ; ebx esp, ebp, esi, edi
     dd 0, 0, 0, 0, 0, 0     ; es, cs, ss, ds, fs, gs
     dd LDT0_SEL, 0x8000000  ; ldt, trace bitmap 
 
+nop
 
-;Kernel 0 stack
-times 128 dd 0          ; 128 * 4 = 512 ; 512 bytes stack space
+;============================================================================================
+task0:
+    
+    mov ax, 0x17       ; Load User Data Segment
+    mov ds, ax
+    mov al, 65         ; Starting char 'A'
+    
+.repeat_alpha:
+    int 0x20           ; 20(hex) == 32 (dec)     
+    
+    mov ecx, 0xffff    ; Delay
+.lp_0:
+    loop .lp_0
+    jmp .repeat_alpha
+
+nop
+
+;================================================================================
+nop
+times 128 dq 0          ; 128 * 4 = 512 ; 512 bytes stack space
 krn_stk0:
+    dd krn_stk0     ; Stack segment offset position
+    dw 0x10         ; Stack segment, same as kernel data seg
 
-;==============================================================Task 1 LDT and TSS
-align 8
+;=====================================================================================================
+nop
 ldt1: 
     dq 0x0000000000000000
     dq 0x00c0fa00000003ff        ; base = 00 ; g=1 x=1 0 avl=0   limit=0| p = 1 DLP=11 s=1 type code=1 conf=0 e/r= 1 a=0          base=00     |  base = 0000 limit = 03ff
     dq 0x00c0f200000003ff        ; stack segment
 
-
+nop
 ; ---------------------------- task 1
 tss1:
     dd 0                                                ; back link
     dd krn_stk1, 0x10                                   ; esp0, ss0
     dd 0, 0, 0, 0, 0                                    ; esp1, ss1, esp2, ss2, cr3
     dd task1, 0x200                                     ; eip, eflags
-    dd 0, 0, 0                                          ; eax, ecx, edx
-    dd 0, usr_stk1, 0, 0, 0                             ; ebx esp, ebp, esi, edi
-    dd 0x17, 0x17, 0x17, 0x17, 0x17, 0x17               ; es, cs, ss, ds, fs, gs
+    dd 0, 0, 0, 0                                       ; eax, ecx, edx, ebx
+    dd usr_stk1, 0, 0, 0                                ; esp, ebp, esi, edi
+    dd 0x17, 0x0f, 0x17, 0x17, 0x17, 0x17               ; es, cs, ss, ds, fs, gs
     dd LDT1_SEL, 0x8000000                              ; ldt, trace bitmap
 
+nop
 
-;---------------------------------------------------------------------Kernel 0 stack
-times 128 dd 0          ; 128 * 4 = 512 ; 512 bytes stack space
-krn_stk1:
-
-;=========================================================== Programs tasks
-task0:
-    mov eax, 0x17
+;=======================================================================================================
+task1:
+    
+    mov ax, 0x17       ; Load User Data Segment
     mov ds, ax
-    mov al, 65
-    int 0x80
-    mov ecx, 0xfff
+    mov al, 'B'         ; Starting char 'B'
+    
+.repeat_alpha:
+    int 0x20           ; 20(hex) == 32 (dec)     
+    
+    mov ecx, 0xffff    ; Delay
 .lp_0:
     loop .lp_0
-    jmp task0   
-;---------------------task1
-task1:
-    mov al, 66
-    int 0x80
-    mov ecx, 0xfff
-.lp_1:
-    loop .lp_1
-    jmp task1 
+    jmp .repeat_alpha
 
-;------------------------------- user stack
-times 128 dd 0          ; 128 * 4 = 512 ; 512 bytes stack space
+nop
+
+;=============================================================================================================
+
+times 128 dq 0          ; 128 * 4 = 512 ; 512 bytes stack space
+krn_stk1:
+
+nop
+
+times 128 dq 0          ; 128 * 4 = 512 ; 512 bytes stack space
 usr_stk1:
+
+nop
